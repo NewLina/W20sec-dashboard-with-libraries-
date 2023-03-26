39fb0736f3dca4d573477b19cf13becde8b6f554
Имя <prybak497@gmail.com>
2023-03-26 16:55:16 +0300

    .

--- assignment2/script/index2.js
+++ assignment2/script/index2.js
@@ -3,39 +3,127 @@
 
 
 const weeklyChart=document.querySelector('.statistics__weekly-chart');
+const myWeeklyChart=document.getElementById('myWeeklyChart');
+const userName=document.querySelector('.title-text');
+const totalProjects = document.querySelector('.total-projects');
+const totalTasks = document.querySelector('.total-tasks');
 
-
-const dataJSON = `
-    {
-            "monday": "4",
-            "tuesday": "2",
-            "wednesday": "6",
-            "thursday": "8",
-            "friday": "5",
-            "saturday": "1",
-            "sunday": "3"
-        }
-    `;
+const dataJSON = `{
+  "name": "Dear Developer",
+  "monday": 4,
+  "tuesday": 2,
+  "wednesday": 6,
+  "thursday": 8,
+  "friday": 5,
+  "saturday": 1,
+  "sunday": 3,
+  "mondayTasks": 0,
+  "newMondayTasks": 4,
+  "tuesdayTasks": 5,
+  "newTuesdayTasks": 2,
+  "wednesdayTasks": 3,
+  "newWednesdayTasks": 0,
+  "thursdayTasks": 6,
+  "newThursdayTasks": 1,
+  "fridayTasks": 2,
+  "newFridayTasks": 4,
+  "saturdayTasks": 7,
+  "newSaturdayTasks": 3,
+  "sundayTasks": 4,
+  "newSundayTasks": 4
+}`;
 
 const data=JSON.parse(dataJSON);
 
-const myWeeklyChart=document.getElementById('myChart');
+userName.textContent=`Welcome ${data.name}!`;
+
 new Chart(myWeeklyChart, {
-    type: 'line',
-    data: {
-      labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-      datasets: [{
-        label: 'Weekly Activity',
-        data: [data.monday, data.tuesday, data.wednesday, data.thursday, data.friday, data.saturday, data.sunday],
-        borderWidth: 3,
-        borderColor: 'rgb(75, 192, 192)',
-    tension: 0.5,
-    
-      }]
-    },
-    options: {
-        layout: {
-            padding: 30
-        }
+  type: 'line',
+  data: {
+    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    datasets: [{
+      label: 'Weekly Activity',
+      data: [data.monday, data.tuesday, data.wednesday, data.thursday, data.friday, data.saturday, data.sunday],
+      borderWidth: 3,
+      borderColor: 'rgb(255, 196, 1)',
+      tension: 0.5 
+    }]
+  },
+  options: {
+      layout: {
+          padding: 30
+      }
+  }
+});
+
+
+const options = {
+  colors:['rgb(255, 218, 9)', 'rgb(221, 146, 34)'],
+  series: [{
+  name: 'New Task',
+  data: [data.newMondayTasks, data.newTuesdayTasks, data.newWednesdayTasks, data.newThursdayTasks, data.newFridayTasks, data.newSaturdayTasks, data.newSundayTasks]
+}, {
+  name: 'In Progress',
+  data: [data.mondayTasks, data.tuesdayTasks, data.wednesdayTasks, data.thursdayTasks, data.fridayTasks, data.saturdayTasks, data.sundayTasks]
+}],
+  chart: {
+  type: 'bar',
+  height: 350,
+  stacked: true,
+  toolbar: {
+    show: true
+  },
+  zoom: {
+    enabled: true
+  }
+},
+responsive: [{
+  breakpoint: 480,
+  options: {
+    legend: {
+      position: 'bottom',
+      offsetX: -10,
+      offsetY: 0
     }
-  });
+  }
+}],
+plotOptions: {
+  bar: {
+    horizontal: false,
+    borderRadius: 10,
+    dataLabels: {
+      total: {
+        enabled: true,
+        style: {
+          fontSize: '13px',
+          fontWeight: 900
+        }
+      }
+    }
+  },
+},
+xaxis: {
+  categories: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+},
+legend: {
+  position: 'right',
+  offsetY: 40
+},
+fill: {
+  opacity: 1
+}
+};
+
+const taskChart = new ApexCharts(document.querySelector(".tasks__chart"), options);
+taskChart.render();
+
+function countProjects() {
+  const projectSum=Math.floor((data.monday + data.tuesday + data.wednesday + data.thursday + data.friday + data.saturday + data.sunday)/10)*10;
+  return projectSum;
+}
+function countTasks() {
+  const taskSum=Math.floor((data.newMondayTasks + data.newTuesdayTasks + data.newWednesdayTasks + data.newThursdayTasks + data.newFridayTasks + data.newSaturdayTasks + data.newSundayTasks + data.mondayTasks + data.tuesdayTasks + data.wednesdayTasks + data.thursdayTasks + data.fridayTasks + data.saturdayTasks + data.sundayTasks)/10)*10;
+  return taskSum;
+}
+
+  
